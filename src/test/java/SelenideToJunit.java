b1c849c8f3209af3b965b9de38374e9582e35a10
Luzan Andrey <adluzluzan2@gmail.com>
2024-10-20 22:24:08 +0300

    end of dz

--- src/test/java/SelenideToJunit.java
+++ src/test/java/SelenideToJunit.java
@@ -4,26 +4,37 @@ import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.text;
-;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideToJunit {
     @BeforeAll
     static void beforeAll() {
-        Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com";
     }
     @Test
-    void andreiSolntsevShouldBeTheFirstContributor() {
+    void SelenideToJunitTest() {
         // открыть страницу репозитория селенида
         open("https://github.com/selenide/selenide");
-        // подвести мышку к первому аватару из блока contributors
+        //Перейти в WIKI
         $("#wiki-tab").click();
-
-
+        // Нажать кнопку для 3 доп вариантов
+        $(".wiki-more-pages-link button").click();
+        // Убедиться, что есть SoftAssertions и нажать на него
+        $$("#wiki-pages-box summary a").findBy(text("SoftAssertions")).shouldBe(visible).click();
+        // Убедиться, что на странице есть пример кода JUnit
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
     }
 }

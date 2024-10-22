5bb4ec9e448a25d74bd879dc374b8e1634756c6d
Luzan Andrey <adluzluzan2@gmail.com>
2024-10-22 17:43:59 +0300

    2nd part of dz

---
+++ src/test/java/DragnDropTest.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragnDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";;
+    }
+    @Test
+    void DragnDropTest() {
+
+        // открыть главную страницу
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        // перемещение местами прямоугольников
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(150, 0).release().perform();
+
+        // проверка, что прямоугольники поменялись
+
+        $("#column-a").shouldHave(text("B"));}
+
+    @Test
+    void NewDragnDropTest() {
+
+        // открыть главную страницу
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        // перемещение местами прямоугольников
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+
+        // проверка, что прямоугольники поменялись
+        $("#column-a").shouldHave(text("B"));;
+
+    }
+}
